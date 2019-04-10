04a6747ebd9e2df3565f0b8fa6bba5a275e6a758
Samuel Shing <samuelshing@gmail.com>
2019-04-10 09:48:20 +0800

    material design

--- app/src/main/java/tw/samuel/guess/MaterialActivity.kt
+++ app/src/main/java/tw/samuel/guess/MaterialActivity.kt
@@ -1,23 +1,34 @@
 package tw.samuel.guess
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_material.*
+import kotlinx.android.synthetic.main.content_material.*
 
-class MainActivity : AppCompatActivity() {
+class MaterialActivity : AppCompatActivity() {
 	val secretNumber = SecretNumber()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-		Log.d(TAG, "secret: ${secretNumber.secret}")
-	}
+		setContentView(R.layout.activity_material)
+		setSupportActionBar(toolbar)
 
-	private val TAG = MainActivity::class.java.simpleName
+		fab.setOnClickListener { view ->
+			AlertDialog.Builder(this)
+				.setTitle("Replay game")
+				.setMessage("Are you sure?")
+				.setPositiveButton(getString(R.string.ok)) { dialog, which ->
+					secretNumber.reset()
+					counter.text = secretNumber.count.toString()
+					ed_number.setText("")
+				}
+				.setNeutralButton("Cancel", null)
+				.show()
+		}
+		counter.text = secretNumber.count.toString()
+	}
 
 	fun check(view: View) {
 		val number = ed_number.text.toString().toInt()
@@ -28,6 +39,7 @@ class MainActivity : AppCompatActivity() {
 		} else if (validate > 0) {
 			message = getString(R.string.smaller)
 		}
+		counter.text = secretNumber.count.toString()
 //		Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 		AlertDialog.Builder(this)
 			.setTitle(getString(R.string.result))
@@ -35,4 +47,5 @@ class MainActivity : AppCompatActivity() {
 			.setPositiveButton(getString(R.string.ok), null)
 			.show()
 	}
+
 }
